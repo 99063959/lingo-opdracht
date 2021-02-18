f325871d49b92c6e63b85c18062187987d8a975a
owen <99063959@mydavinci.nl>
2021-02-18 10:03:39 +0100

    opdracht

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ for(var j=1;j<=5;j++){
 }
 
 function randomWoord(){
-	randomWoord = words[Math.floor(Math.random() * words.length)];
-	console.log(randomWoord);
+	//randomWoord = words[Math.floor(Math.random() * words.length)];
+	randomWoord = "kaars";
+    console.log(randomWoord);
 }
 
 function eersteLetter(){
@@ -36,32 +37,43 @@ function woordSplit(woord){
 }
 
 function woordCheck(woord){
-	pogingen++
-	woordSplit(woord);
-	if(pogingen >=5){
-		alert("verloren te veel pogingen");
-		location.reload();
-	}
-	if(woord == randomWoord){
-		alert("goed geraden");
-		location.reload();
-	}
-	
-	for(var i=0;i<inputletters.length;i++){
-		document.getElementById("woord" + pogingen + "letter" + (i+1)).innerHTML = "<h1></h1>" + inputletters[i];
+    pogingen++
+    woordSplit(woord);
+    if(pogingen>=5){
+        alert("verloren teveel pogingen");
+        location.reload();
+    }
+    if(woord==randomWoord){
+        alert("goed geraden");
+        location.reload();
+    }
+    
+    for(var i=0; i<inputletters.length; i++){
+        document.getElementById("woord" + pogingen + "letter" +(i+1)).innerHTML="<h1></h1>"+ inputletters[i];
 
-		if(inputletters[i]== woordletters[i]){
-			document.getElementById("woord" + pogingen + "letter" + (i+1)).style.backgroundColor = "green";
-			letterCOPY[i]= null;
-			console.log(letterCOPY);
-		}else{
-			if(letterCOPY.includes(inputletters[i])){
-				console.log(letterCOPY);
-				document.getElementById("woord" + pogingen + "letter" + (i+1)).style.backgroundColor = "yellow";
-				document.getElementById("woord" + pogingen + "letter" + (i+1)).style.borderRadius = "50%";
-			}
-		}
-	}
+        if(inputletters.includes(woordletters[i])){
+            document.getElementById("woord" + pogingen + "letter" +(i+1)).style.backgroundColor="yellow";
+            document.getElementById("woord" + pogingen + "letter" +(i+1)).style.borderRadius="50%";
+            //inputletters[i] = "";
+            console.log(woordletters[i] + "geel");
+            woordletters[i] = "";
+        }
+        console.log(inputletters[i] + "geel buiten iffie");
+    }
+
+    woordSplit(woord);
+
+    for(var i=0; i<inputletters.length; i++){
+        document.getElementById("woord" + pogingen + "letter" +(i+1)).innerHTML="<h1></h1>"+ inputletters[i];
+
+        if(inputletters[i] == woordletters[i]){
+            document.getElementById("woord" + pogingen + "letter" +(i+1)).style.backgroundColor="green";
+            console.log(woordletters[i]);
+            console.log(woordletters[i] + "groen");
+        }
+        console.log(woordletters[i] + "groen buiten iffie");
+        woordletters[i] = "";
+    }
 }
 
 randomWoord();
